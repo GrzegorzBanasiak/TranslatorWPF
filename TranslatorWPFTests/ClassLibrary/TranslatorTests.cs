169d03779075406b6682950d017cd327dea0fbf8
Chopfl <claudiahopfl@gmail.com>
2019-02-10 19:14:15 +0100

    Dodanie klas testów dla pozostałych klas

---
+++ TranslatorWPFTests/ClassLibrary/TranslatorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TranslatorWPFTests.ClassLibrary
+{
+    [TestClass]
+    public class TranslatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
