30a1c6e21138b8ee036dcd6b07b0153892674de5
Chopfl <claudiahopfl@gmail.com>
2019-02-10 19:59:27 +0100

    Usunięcie niepotrzebych odwołań

--- TranslatorWPFTests/ClassLibrary/TextGetterTests.cs
+++ TranslatorWPFTests/ClassLibrary/TextGetterTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TranslatorWPF;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TranslatorWPF.Tests
 {
