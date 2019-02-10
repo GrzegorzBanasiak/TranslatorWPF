90e90655dc66fbaa546e2bf3ab3a386bc35d913e
GrzegorzBanasiak <grzegorzbanasiak95@gmail.com>
2019-02-10 19:08:56 +0100

    Test dla klasy Settings

--- TranslatorWPF/ClassLibrary/Settings.cs
+++ TranslatorWPF/ClassLibrary/Settings.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TranslatorWPF.ClassLibrary
+namespace TranslatorWPF
 {
     /// <summary>
     /// Get Api key 
     /// </summary>
-    static class Settings
+    public static class Settings
     {
         public static String API_KEY = "trnsl.1.1.20190122T131408Z.7e8f1dd71efa4d37.2d3037c7e197068496a8cfca764dc21812229fad";
     }
---
+++ TranslatorWPFTests/ClassLibrary/SettingsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TranslatorWPF;
+
+namespace TranslatorWPFTests.ClassLibrary
+{
+    [TestClass]
+    public class SettingsTests
+    {
+        [TestMethod]
+        public void GetKeyTest()
+        {
+            String actual;
+            String expected = "trnsl.1.1.20190122T131408Z.7e8f1dd71efa4d37.2d3037c7e197068496a8cfca764dc21812229fad";
+
+            actual = Settings.API_KEY;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetKeyTestNotEqual()
+        {
+            String actual;
+            String expected = "68496a8cfca764dc21812229fad";
+
+            actual = Settings.API_KEY;
+
+            Assert.AreNotEqual(expected, actual);
+        }
+    }
+}
